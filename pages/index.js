880de2c2f62b14ac8cba058821d6fdbad1e7eaef
Nick van der Raaf <nickraaf@outlook.com>
2022-10-24 22:45:46 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,8 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Bjorn is een balletje bv
+          {/* Welcome to <a href="https://nextjs.org">Next.js!</a> */}
         </h1>
 
         <p className={styles.description}>
